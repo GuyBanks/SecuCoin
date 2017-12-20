1187e4f78bc0aa3313ab9116f54b0e9ef08ef714
GuyBanks <dylncox@gmail.com>
2017-12-19 21:59:32 -0600

    Changed first line comment

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2016 The StratCoin developers
+// Copyright (c) 2017-2018 The ShipCoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
