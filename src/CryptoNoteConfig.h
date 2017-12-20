50f85ca856cdc67d5a15958823f17ce3943d7b7c
GuyBanks <dylncox@gmail.com>
2017-12-19 22:05:01 -0600

    Update seed note IP from Vultr server

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = { 
-  "111.11.11.11:18412",
-  "222.22.22.22:18263",
+  "207.246.73.16",
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
 };
